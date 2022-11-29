19ee532d67b2550704afb46998423524c894043a
Alejo Lasso <alejolassoe1941@gmail.com>
2022-11-29 14:24:16 -0300

    reac

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import Carusel from './components/Carusel/Carusel';
 
 function App() {
+
+  let styles = {
+    color: "white",
+    backgroundColor: "blue"
+  }
+
+
   return (
-    <div className="App">
+    <div>
+      <NavBar/>
+      <Carusel/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> 
+        <h1 style={styles}Mi nombre es></h1>
         <a
           className="App-link"
           href="https://reactjs.org"

d05e36699bcb95f210a1ea78cd1ab0a91e8057d7
Alejo Lasso <alejolassoe1941@gmail.com>
2022-12-01 18:32:38 -0300

    agregando cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Card = (props) => {
     const {titulo, descripcion, precio, img, btnText} = props
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4">
+    <div className="row row-cols-1 row-cols-md-3 g-4 ">
   <div className="col">
-    <div className="card h-100">
+    <div className="card h-100 d-flex">
       <img src={img} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">{titulo}</h5>
